086ee7fe2fe49f29b6862f3c05ee6bc00c62ef64
Lissy02 <danylissy2002@gmail.com>
2023-05-23 19:21:05 -0700

    actual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// importacion de rutas
+import {HomeComponent} from './componentes/home/home.component';
+import { EquipoComponent } from './componentes/equipo/equipo.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path:'',component:HomeComponent},
+  {path:'equipo',component:EquipoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
